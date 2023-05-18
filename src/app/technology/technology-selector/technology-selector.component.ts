edb4c247529a66bfc36892e94bfc2d558181f805
Johnny Gérard <john@jgerard.dev>
2023-05-18 13:23:09 +0200

    feat: technology-selector component

--- src/app/technology/technology-selector/technology-selector.component.ts
+++ src/app/technology/technology-selector/technology-selector.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+enum Technology {
+  LAUNCH_VEHICLE = 1,
+  SPACEPORT,
+  SPACE_CAPSULE
+}
+
 @Component({
   selector: 'app-technology-selector',
   templateUrl: './technology-selector.component.html',
   styleUrls: ['./technology-selector.component.scss']
 })
 export class TechnologySelectorComponent {
+  technology: Technology = Technology.LAUNCH_VEHICLE;
 
+  protected setTechnology(n: Technology) {
+    this.technology = n;
+  }
 }
