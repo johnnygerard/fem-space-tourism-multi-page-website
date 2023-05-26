9dc319f2a8cc851f92eee26fd01e535e6f46ced9
Johnny Gérard <john@jgerard.dev>
2023-05-26 21:52:16 +0200

    feat: add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { TechnologySelectorComponent } from './technology/technology-selector/technology-selector.component';
@@ -16,7 +17,8 @@ import { SrcsetDirective } from './srcset.directive';
     SrcsetDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
