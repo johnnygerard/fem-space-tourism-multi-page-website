8f6ec2cf38d0c0dd9e17cd1d1fa79c15dfbd50c5
Johnny Gérard <john@jgerard.dev>
2023-06-02 23:06:15 +0200

    feat: crew page component

--- src/app/crew/crew.module.ts
+++ src/app/crew/crew.module.ts
@@ -1,29 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectorComponent } from './selector/selector.component';
-import { SELECTION_TOKEN, SelectionService } from '../selection.service';
 import { HeroComponent } from './hero/hero.component';
 import { SharedModule } from '../shared/shared.module';
 import { BioComponent } from './bio/bio.component';
-
-
+import { CrewComponent } from './crew/crew.component';
 
 @NgModule({
   declarations: [
     SelectorComponent,
     HeroComponent,
-    BioComponent
+    BioComponent,
+    CrewComponent
   ],
   imports: [
     CommonModule,
     SharedModule
   ],
   exports: [
-    BioComponent
+    CrewComponent
   ],
-  providers: [
-    SelectionService,
-    { provide: SELECTION_TOKEN, useValue: { length: 4, interval: 1e3 } }
-  ]
 })
 export class CrewModule { }
