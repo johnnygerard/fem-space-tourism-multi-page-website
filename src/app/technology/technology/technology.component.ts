4c15c43579e49c70846edcbb3ff7a09b4fba32e3
Johnny Gérard <john@jgerard.dev>
2023-06-02 11:14:18 +0200

    fix: service provider conflict (#16)

--- src/app/technology/technology/technology.component.ts
+++ src/app/technology/technology/technology.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { SELECTION_TOKEN, SelectionService } from 'src/app/selection.service';
 import { Pages } from 'src/app/shared/title/title.component';
 
 @Component({
   selector: 'app-technology',
   templateUrl: './technology.component.html',
-  styleUrls: ['./technology.component.scss']
+  styleUrls: ['./technology.component.scss'],
+  providers: [
+    SelectionService,
+    { provide: SELECTION_TOKEN, useValue: { length: 3, interval: 1e4 } }
+  ]
 })
 export class TechnologyComponent {
   protected readonly page = Pages.TECHNOLOGY;
