d37675450083675e0b915a46b5a7d95095996702
Johnny Gérard <john@jgerard.dev>
2023-06-03 13:07:06 +0200

    feat: destination hero component (#19)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { TechnologyModule } from './technology/technology.module';
 import { NavigationModule } from './navigation/navigation.module';
 import { CrewModule } from './crew/crew.module';
+import { DestinationModule } from './destination/destination.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CrewModule } from './crew/crew.module';
     AppRoutingModule,
     TechnologyModule,
     NavigationModule,
-    CrewModule
+    CrewModule,
+    DestinationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
