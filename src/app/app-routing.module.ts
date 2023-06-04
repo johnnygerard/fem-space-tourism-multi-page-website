27892246dd0c8f2b15ab6deddff70d11efa6c762
Johnny Gérard <john@jgerard.dev>
2023-06-04 21:20:57 +0200

    feat: root component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TechnologyComponent } from './technology/technology/technology.component';
+import { HomeComponent } from './home/home/home.component';
+import { DestinationComponent } from './destination/destination/destination.component';
+import { CrewComponent } from './crew/crew/crew.component';
 
 // Dummy component for testing purposes
 @Component({
@@ -10,9 +13,9 @@ import { TechnologyComponent } from './technology/technology/technology.componen
 export class DummyComponent { }
 
 const routes: Routes = [
-  { path: '', component: DummyComponent },
-  { path: 'destination', component: DummyComponent },
-  { path: 'crew', component: DummyComponent },
+  { path: '', component: HomeComponent },
+  { path: 'destination', component: DestinationComponent },
+  { path: 'crew', component: CrewComponent },
   { path: 'technology', component: TechnologyComponent },
   { path: '**', component: DummyComponent },
 ];
