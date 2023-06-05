427d6db191a8bf3faf7f1520aee614436333fb5c
Johnny Gérard <john@jgerard.dev>
2023-06-05 18:35:03 +0200

    feat: home page redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,19 +5,12 @@ import { HomeComponent } from './home/home/home.component';
 import { DestinationComponent } from './destination/destination/destination.component';
 import { CrewComponent } from './crew/crew/crew.component';
 
-// Dummy component for testing purposes
-@Component({
-  selector: 'app-dummy',
-  template: `<p>dummy works!</p>`
-})
-export class DummyComponent { }
-
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'destination', component: DestinationComponent },
   { path: 'crew', component: CrewComponent },
   { path: 'technology', component: TechnologyComponent },
-  { path: '**', component: DummyComponent },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
