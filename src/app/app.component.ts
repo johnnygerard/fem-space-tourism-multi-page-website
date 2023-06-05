bed0c2dbcdaf6e4a4fc74e9dcd93e644cd0cd6fb
Johnny Gérard <john@jgerard.dev>
2023-06-05 16:41:48 +0200

    feat: attribution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    if (environment.production) {
+      console.log(`Challenge by Frontend Mentor. Coded by Johnny Gérard.
+
+Portfolio: https://www.frontendmentor.io/profile/johnnygerard`);
+    }
+  }
+}
